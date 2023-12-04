e7e332c3bac31ea7eb43f156a3139cabeba1934f
belennazareth <belennazareth29@gmail.com>
2023-12-04 22:11:48 +0100

    changes

--- src/org/nazaret/Dns.groovy
+++ src/org/nazaret/Dns.groovy
@@ -5,8 +5,8 @@ Void dnsK8s(String tag) {
     def node_ip = sh(script: 'kubectl get nodes -o jsonpath="{.items[?(@.metadata.labels.kubernetes\\.io/hostname==\'ubuntubn\')].status.addresses[?(@.type==\'InternalIP\')].address}"', returnStdout: true).trim()
     def isEntryExists = sh(script: "grep -q \"${domain}\" /etc/hosts && echo 'true' || echo 'false'", returnStatus: true).exitCode == 0
     if (isEntryExists != "true") {
-        sh "echo \"${node_ip} ${domain}\" | sudo tee -a /etc/hosts"
         println "Dirección agregada a /etc/hosts"
+        sh "echo \"${node_ip} ${domain}\" | sudo tee -a /etc/hosts"
     } else {
         println "La dirección ya existe en /etc/hosts"
     }    
