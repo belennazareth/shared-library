6866f93df5c39b355a5391ed857635dd09256059
belennazareth <belennazareth29@gmail.com>
2023-12-04 20:52:34 +0100

    modified

--- src/org/nazaret/Deploy.groovy
+++ src/org/nazaret/Deploy.groovy
@@ -3,7 +3,7 @@ package org.nazaret
 Void deployK8s(String tag) {
     sh "mkdir -p ${env.WORKSPACE}/.kube"
     sh "sudo cp -i /etc/kubernetes/admin.conf ${env.WORKSPACE}/.kube/config"
-    sh "sudo chown $(id -u):$(id -g) ${env.WORKSPACE}/.kube/config"
+    sh "sudo chown \$(id -u):\$(id -g) ${env.WORKSPACE}/.kube/config"
     sh "cd chart &&
     helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag}"
 }
