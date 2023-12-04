a43d81c3879948fa086d5d806c9a6e77d66d8460
belennazareth <belennazareth29@gmail.com>
2023-12-04 20:47:29 +0100

    added k8s login

--- src/org/nazaret/Deploy.groovy
+++ src/org/nazaret/Deploy.groovy
@@ -1,7 +1,13 @@
 package org.nazaret
 
 Void deployK8s(String tag) {
-    sh "helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag}"
+    
+    sh "mkdir -p ${env.WORKSPACE}/.kube
+        sudo cp -i /etc/kubernetes/admin.conf ${env.WORKSPACE}/.kube/config
+        sudo chown $(id -u):$(id -g) ${env.WORKSPACE}/.kube/config"
+    
+    sh "cd chart &&
+    helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag}"
 }
 
 return this
