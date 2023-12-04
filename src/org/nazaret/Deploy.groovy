9b1b90cab9f05b9530d9b0ba8f4dd252dbf53d9b
belennazareth <belennazareth29@gmail.com>
2023-12-04 21:42:38 +0100

    added dynamic domain

--- src/org/nazaret/Deploy.groovy
+++ src/org/nazaret/Deploy.groovy
@@ -1,10 +1,11 @@
 package org.nazaret
 
 Void deployK8s(String tag) {
+    String domain = "${tag}.belennazaret.org"
     sh "mkdir -p ${env.WORKSPACE}/.kube"
     sh "sudo cp -i /etc/kubernetes/admin.conf ${env.WORKSPACE}/.kube/config"
     sh "sudo chown \$(id -u):\$(id -g) ${env.WORKSPACE}/.kube/config"
-    sh "cd chart && helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag}"
+    sh "cd chart && helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag} --set 'ingress.hosts[0].host=${domain}'"
 }
 
 return this
