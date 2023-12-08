e445659d25cc0442a5a926a8f1d487edcd3ade23
belennazareth <belennazareth29@gmail.com>
2023-12-08 19:27:09 +0100

    changes namespace existe

--- src/org/nazaret/Deploy.groovy
+++ src/org/nazaret/Deploy.groovy
@@ -2,10 +2,23 @@ package org.nazaret
 
 Void deployK8s(String tag) {
     String domain = "${tag}.belennazaret.org"
+    Boolean restart = false
+
     sh "mkdir -p ${env.WORKSPACE}/.kube"
     sh "sudo cp -i /etc/kubernetes/admin.conf ${env.WORKSPACE}/.kube/config"
     sh "sudo chown \$(id -u):\$(id -g) ${env.WORKSPACE}/.kube/config"
-    sh "cd chart && helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag} --set 'ingress.hosts[0].host=${domain}'"
+
+    def namespaceExiste = sh(script: "kubectl get namespace ${tag}", returnStatus: true)
+
+    if (namespaceExiste == "0") {
+        restart = true
+    }
+
+    if (restart) { 
+        sh "kubectl rollout restart deployment app-chart -n ${tag}"
+    } else {
+        sh "cd chart && helm upgrade app --install -f values.yaml . -n ${tag} --create-namespace --set image.tag=${tag} --set 'ingress.hosts[0].host=${domain}'"
+    }
 }
 
 return this
